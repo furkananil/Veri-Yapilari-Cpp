0f908a2f2b48721da732181b78f605cf7f92228e
furkananil <furkananil960@gmail.com>
2024-11-14 13:47:26 +0300

    Dairesel Iki Yonlu Bagli Liste

---
+++ DaireselIkiYonluBagliListe/src/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip>
+#include "DaireselIkiYonluBagliListe.hpp"
+using namespace std;
+
+int main()
+{
+	BagliListe<int> liste;
+
+	for (int i = 0; i < 5; i++)
+		liste.ekle((i + 1) * 11);
+	cout << liste;
+	liste.ekle(4, 88);
+	cout << liste;
+   
+    return 0;
+}
